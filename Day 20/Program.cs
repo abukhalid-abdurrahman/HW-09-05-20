b40c032fc58294a0312e638f8e8dafe99229f03d
faha-berdiev <madysoftware@gmail.com>
2020-05-10 02:50:24 +0300

    Задание 2.1

--- Day 20/Program.cs
+++ Day 20/Program.cs
@@ -8,6 +8,21 @@ namespace Day_20
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Task 20...");
+
+            //Задание 2.1
+            int randNumber = new Random().Next(300000, 500000);
+            string numbersStr = randNumber.ToString();
+            var editedList = from elem in numbersStr
+                             select elem;
+            editedList = editedList.Reverse();
+
+            Console.Write(randNumber + "\t[");
+            foreach (var item in editedList)
+                Console.Write($"{item}, ");
+            Console.Write("]");
+
+            Console.Read();
         }
     }
 }
